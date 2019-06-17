fda1e5c1db2730a9fce9f1e941dea52cb9971837
NDF96 <51333291+NDF96@users.noreply.github.com>
2019-06-17 13:27:05 +0200

    added a numpy input adNoise

--- addNoise.py
+++ addNoise.py
@@ -43,3 +43,48 @@ def addNoise(wav_signal_name, wav_noise_name, outputName = "noisy_output.wav", s
     return noisy, Fc_s
 
 
+    # this version takes in input numpy vectors instead of .wav files
+def addNoise1(signal, noise, start = 0, randomized = False, intensity = 1):
+    # adds noise to a signal
+    # same sample frequency for both signals is needed
+    # inputs: signal to make a noisy one, noise signal, name of the output, intensity is how much amplified we want the noise to be.
+    # if randomized == True, the part of noise signal to be added is chosen randomly
+    # start = where(which sample) to start getting the noise from the noise signal
+    # output: noisy signal
+
+    length = signal.shape[0]  # being the audios 1 second long it should be always equal to Fc_s
+    
+    h = noise.shape[0]-length # max index to start taking the noise signal
+    if(start > h):
+        raise ValueError("start exceeds array bounds")
+    
+    if(randomized):
+        # choose start randomly in the allowed range
+        start = np.random.randint(0, high = h)
+    
+    noise = noise[start:start + length]
+    # control how much noise to add
+    additive_noise = (np.multiply(intensity, noise))
+
+    noisy = np.add(signal, additive_noise);
+    if(toWrite):
+        write(outputName, Fc_s, noisy)
+
+    return noisy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
